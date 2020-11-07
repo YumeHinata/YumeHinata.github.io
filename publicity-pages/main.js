3cfb6bf05ea6f7302dfc0318fab3fbc29f9f1a28
YumeHinata <yume3099729829@gmail.com>
2020-11-07 14:07:05 +0800

    Site updated: 2020-11-07 14:07:04

---
+++ publicity-pages/main.js
@@ -0,0 +1,15 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script><script class="meting-secondary-script-marker" src="\assets\js\Meting.min.js"></script>document.getElementById("button1").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
+
+document.getElementById("button2").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
+
+document.getElementById("button3").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
+
+document.getElementById("button4").onclick=function(){
+	window.open("https://www.bakary.cn/");
+}
